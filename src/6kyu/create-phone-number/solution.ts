8397b1569b317d2b0c2c705aa2b104eebf9270a5
Rick Mather <rick.mather@cef.co.uk>
2023-08-11 15:10:24 +0100

    feat: solve create phone number

--- src/6kyu/create-phone-number/solution.ts
+++ src/6kyu/create-phone-number/solution.ts
@@ -1,3 +1,7 @@
 export function createPhoneNumber(numbers: number[]): string {
-  return '';
+  const areaCode = numbers.slice(0, 3).join('');
+  const telephonePrefix = numbers.slice(3, 6).join('');
+  const lineNumber = numbers.slice(6, 10).join('');
+
+  return `(${areaCode}) ${telephonePrefix}-${lineNumber}`;
 }
